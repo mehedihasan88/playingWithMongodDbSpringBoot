de057ed6a8f7d7fb2d3f2f3324aa729628f9f513
Mehedi Hasan <mmehedihasan29088@gmail.com>
2023-10-07 19:30:56 +0600

    initial files

---
+++ MongDbSpringBoot/src/main/java/com/example/MongDbSpringBoot/MongDbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.MongDbSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongDbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongDbSpringBootApplication.class, args);
+	}
+
+}
---
+++ MongDbSpringBoot/src/test/java/com/example/MongDbSpringBoot/MongDbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MongDbSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongDbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
